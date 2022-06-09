f1f374a661b6e6af88710d6cae6adb188986b8e7
Mishelle Iturriaga <mishelleiturriaga@gmail.com>
2022-06-09 10:07:10 -0400

    Update browser.py

--- basefunc/browser.py
+++ basefunc/browser.py
@@ -1,15 +1,13 @@
-import sys
-import time
-import os
-from sys import platform
 
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import time
+
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 from basefunc.driver import Driver
 from basefunc.config import TEST_CONFIG
-from selenium.webdriver.support import expected_conditions as EC, expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -70,7 +68,6 @@ class Browser(object):
     def get_screenshot(self, screenshot_name='screenshot.png'):
         self.driver.save_screenshot(TEST_CONFIG['screenshotsfolder']+"/"+screenshot_name)
 
-
     def element_present(self, xpath):
         self.check_elem_state(xpath)
         try:
@@ -95,21 +92,7 @@ class Browser(object):
     def click_create_an_account_button(self):
         self.click_when_element_is_present(dict_vars['CREATE AN ACCOUNT button'])
 
-    def fill_up_form(self, first_name='name', last_name='last name', email='test@gmail.com', password='pw', yes_investor=True):
-        assert self.is_text_appeared_on_page(text=UI_PAGE_TITLE)
-        self.driver.find_element_by_id(dict_vars['First name field']).send_keys(first_name)
-        self.driver.find_element_by_id(dict_vars['Last name field']).send_keys(last_name)
-        self.driver.find_element_by_id(dict_vars['Email address field']).send_keys(email)
-        self.driver.find_element_by_id(dict_vars['Password field']).send_keys(password)
-        self.driver.find_element_by_id(dict_vars['Confirm Password field']).send_keys(password)
-        if yes_investor is True:
-            self.click_when_element_is_present(dict_vars['Yes radio button'])
-        else:
-            self.click_when_element_is_present(dict_vars['No radio button'])
-        self.keyboard_action(times=2)
-        time.sleep(1)
-        self.driver.find_element_by_id(dict_vars['I have read and accept checkbox']).click()
-
+    def checked_not_robot_frame(self):
         '''save the main window handle'''
         main_window = self.driver.current_window_handle
 
@@ -123,10 +106,20 @@ class Browser(object):
 
         ''' navigate back to main window '''
         self.driver.switch_to.window(main_window)
-        
-        ''' all sleep can be replaced by explicit wait '''
-        time.sleep(10)
+
+    def fill_up_form(self, first_name='name', last_name='last name', email='test@gmail.com', password='pw', yes_investor=True):
+        assert self.is_text_appeared_on_page(text=UI_PAGE_TITLE)
+        self.driver.find_element_by_id(dict_vars['First name field']).send_keys(first_name)
+        self.driver.find_element_by_id(dict_vars['Last name field']).send_keys(last_name)
+        self.driver.find_element_by_id(dict_vars['Email address field']).send_keys(email)
+        self.driver.find_element_by_id(dict_vars['Password field']).send_keys(password)
+        self.driver.find_element_by_id(dict_vars['Confirm Password field']).send_keys(password)
+        if yes_investor is True:
+            self.click_when_element_is_present(dict_vars['Yes radio button'])
+        else:
+            self.click_when_element_is_present(dict_vars['No radio button'])
+        self.keyboard_action(key=Keys.PAGE_DOWN, times=1)
+        time.sleep(1)
+        self.driver.find_element_by_id(dict_vars['I have read and accept checkbox']).click()
+        self.checked_not_robot_frame()
         self.click_when_element_is_present(dict_vars['Create an Account button'])
-
-
-
